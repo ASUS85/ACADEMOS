65b50eee7827a0ab39a36a904d4d6f7e2ba530ca
ASUS85 <armandchristian85@gmail.com>
2026-02-14 15:18:02 +0100

    Passage de index.html a index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACADEMOS</title>
+</head>
+<body>
+    <div>GitHub Codespaces</div>
+    <div><h1>Bienvenue sur ACADEMOS</h1></div>
+</body>
+</html>
